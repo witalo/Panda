f538df61c7363b86542e63e3af7448d0617deab0
witalo <vent.wivf@gmail.com>
2024-09-24 16:07:39 -0500

    login

--- app/src/main/java/com/example/panda/LoginScreen.kt
+++ app/src/main/java/com/example/panda/LoginScreen.kt
@@ -3,9 +3,15 @@ package com.example.panda
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,7 +28,25 @@ fun LoginScreen() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(painter = painterResource(id= R.drawable.logo), contentDescription = "Login Panda",
-            modifier = Modifier.size(200.dp))
+            modifier = Modifier.size(150.dp))
         Text(text = "Bienvenido a Panda", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = "Iniciar Sesión", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+        OutlinedTextField(value = "", onValueChange = {}, label = {
+            Text(text = "Correo")
+        })
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value = "", onValueChange = {}, label = {
+            Text(text = "Contraseña")
+        })
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {}) {
+            Text(text = "Ingresar")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        TextButton(onClick = {}) {
+            Text(text = "¿Recuperar contraseña?")
+        }
+
     }
 }
