6bc93bf4560b04ff732b3d22c63629aee5cb37b2
witalo <vent.wivf@gmail.com>
2024-09-24 15:57:08 -0500

    cambios panda

---
+++ app/src/main/java/com/example/panda/LoginScreen.kt
@@ -0,0 +1,28 @@
+package com.example.panda
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun LoginScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(id= R.drawable.logo), contentDescription = "Login Panda",
+            modifier = Modifier.size(200.dp))
+        Text(text = "Bienvenido a Panda", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+    }
+}
--- app/src/main/java/com/example/panda/MainActivity.kt
+++ app/src/main/java/com/example/panda/MainActivity.kt
@@ -18,30 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PandaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            LoginScreen()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PandaTheme {
-        Greeting("Android")
     }
 }
